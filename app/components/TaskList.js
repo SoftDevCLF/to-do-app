aa7d91c1163a5dcc237078425c001a86cd3576f3
Temi Bankole <temibankole5@gmail.com>
2025-12-12 22:23:01 -0700

    task list component structure

--- app/components/TaskList.js
+++ app/components/TaskList.js
@@ -1,4 +1,15 @@
-//TODO: DISPLAY A LIST OF TASKS
-//       RECEIVE TASKS FROM PARENT
-//       FILTER TASKS BASED ON CURRENT TAB (ALL, COMPLETED, INCOMPLETE)
-//       RENDER EACH TASK USING TASKCARD
+"use client";
+import TaskCard from "./TaskCard";
+import tasks from "../testingpage/tasks.json";
+
+export default function TaskList() {
+  console.log("Loaded tasks:", tasks); // debug check
+
+  return (
+    <div className="p-4 max-w-md mx-auto">
+      {tasks.map(task => (
+        <TaskCard key={task.id} task={task} />
+      ))}
+    </div>
+  );
+}
