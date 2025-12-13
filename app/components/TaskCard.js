616db7c476ef20ab45da0f71359f14744a82a804
Temi Bankole <temibankole5@gmail.com>
2025-12-12 22:22:29 -0700

    building Task Card component with checkbox and google image icons

--- app/components/TaskCard.js
+++ app/components/TaskCard.js
@@ -1,4 +1,20 @@
-//TODO: DISPLAY A SINGLE TASK
-//       SHOW TASK TITLE
-//       SHOW IF TASK IS COMPLETED OR NOT (OPTIONAL STRIKETHROUGH OR CHECKMARK)
-//       HANDLE TOGGLE OR DELETE IF NEEDED
+"use client";
+
+import deleteIcon from "../assets/trash2.0.png";
+import editIcon from "../assets/edit.png";
+import Image from "next/image";
+
+export default function TaskCard({ task }) {
+  return (
+    <div className="flex items-center p-3 mb-3 border rounded-lg shadow-md bg-blue-200 text-black hover:bg-blue-300 transition-colors">
+      <input
+        type="checkbox"
+        checked={task.completed || false} readOnly
+        className="w-6 h-3"
+      />
+      <div className="flex-1">{task.title}</div>
+      <Image src={editIcon} alt="Edit Task" width={20} height={20} className="cursor-pointer" />
+      <Image src={deleteIcon} alt="Delete Task" width={20} height={20} className="cursor-pointer" />
+    </div>
+  );
+}
