3c791f209f72f3c7eea97bf003554db1e0675ed6
SoftDevCLF <cintya.lara.f@gmail.com>
2025-12-14 01:20:08 -0700

    Create footer

---
+++ app/components/footer.js
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <div className="w-full bg-[#000024] border-t border-[#011D33] py-4">
+      <p className="text-center text-[#F1FAF5] ">
+        {new Date().getFullYear()}. Listo: A project for CPRG 306 Web
+        Application Development II course at SAIT.
+      </p>
+    </div>
+  );
+}
