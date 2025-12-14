5cae9d629218b44d0200a91f59b9ad996e42cc5d
monic <moniclee421@gmail.com>
2025-12-14 14:54:19 -0700

    Merge Cinthya branch into Monic

--- app/page.js
+++ app/page.js
@@ -1,65 +1,47 @@
+"use client";
+import NavigationBar from "./components/nav-bar";
+import Footer from "./components/footer";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+  const loginPageNav = () => {
+    router.push("/tasks-list/login");
+  };
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.js file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
+    <div className="flex flex-col h-screen items-center justify-center bg-[#000024] font-sans">
+      <NavigationBar />
+      <main className="flex max-w-screen w-full items-center py-12 bg-[#000024] sm:items-start">
+        <section className="flex w-full text-center sm:text-left gap-5">
+          <div className="w-full ps-16">
+            <h1 className="text-5xl font-bold mb-6 text-[#F1FAF5]">
+              Focus on what’s next.
+              <br /> One task at a time.
+            </h1>
+            <p className="text-lg mb-8 text-[#F1FAF5]">
+              Organize your tasks efficiently and boost your productivity with
+              our intuitive To Do WebApp: Listo.
+            </p>
+            <button
+              className="px-6 py-3 bg-[#F15A2B] text-white rounded-lg hover:bg-[#D14A1F]"
+              onClick={loginPageNav}
             >
-              Templates
-            </a>{" "}
-            or the {" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+              Get Started
+            </button>
+          </div>
+          <div className="w-full hidden md:block">
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
+              src="/todo4.jpg"
+              alt="To Do WebApp"
+              width={800}
+              height={500}
+              className="rounded-lg"
             />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
+          </div>
+        </section>
       </main>
+      <Footer />
     </div>
   );
 }
