dbafc16afb505e5fdb1d4a4ee5ee2cab44f8b34d
Temi Bankole <temibankole5@gmail.com>
2025-12-14 20:57:29 -0700

    task form rendered on page

--- app/testingpage/page.js
+++ app/testingpage/page.js
@@ -1,11 +1,13 @@
-import TaskCard from "../components/TaskCard";
+import TaskForm from "../components/TaskForm";
 import TaskList from "../components/TaskList";
+
 
 //Page
 export default function Page() {
   return (
     <main className="space-y-2 text-white">
       <h1 className="text-black text-center text-5xl">All Tasks</h1>
+      <TaskForm/>
       <TaskList/>
       </main>
   );
