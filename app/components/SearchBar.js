e73f6ac88ea7bcfd5235deb7b88eda8f387110d0
Temi Bankole <temibankole5@gmail.com>
2025-12-15 16:32:02 -0700

    Search bar implementation to search through task list

--- app/components/SearchBar.js
+++ app/components/SearchBar.js
@@ -1,3 +1,17 @@
-//TODO: USER CAN SEARCH TASKS BY TITLE
-//       AS USER TYPES, TASK LIST FILTERS TO SHOW MATCHING TASKS
-//       ONLY TASKS WITH TITLES THAT MATCH THE SEARCH QUERY SHOULD BE DISPLAYED
+"use client";
+
+export default function SearchBar({ value, onChange}) {
+  return (
+    <div className="ml-0 mb-5 space-y-6">
+    <input
+      type="text" 
+      value={value}
+      onChange={(e) => onChange(e.target.value)}
+      placeholder="Search tasks..."
+      className="flex-1 px-4 py-3 rounded-xl bg-[#000024] text-[#F1FAF5]
+          border border-[#F15A2B] placeholder:text-[#92dad7]
+          focus:outline-none focus:ring-2 focus:ring-[#92dad7]"
+    />
+    </div>
+  );
+}
