1bd1679c41405276574b24c7b2d288b1fbfb8ef3
Temi Bankole <temibankole5@gmail.com>
2025-12-15 01:31:52 -0700

    Adding delete button for cleaner UI and styling to match web app

--- app/components/TaskCard.js
+++ app/components/TaskCard.js
@@ -1,18 +1,23 @@
 "use client";
 
-import deleteIcon from "../assets/trash2.0.png";
-import Image from "next/image";
-
+// TaskCard component displays a single task item
 export default function TaskCard({ task }) {
   return (
-    <div className="flex items-center p-3 mb-3 border rounded-lg shadow-md bg-blue-200 text-black">
+    <div className="flex items-center justify-between w-full p-8 mb-4 border border-[#F15A2B] rounded-2xl bg-[#01013D] text-white shadow-lg hover:shadow-xl hover:scale-105 transition-all ">
+      <div className="flex items-center gap-4 py-6">
       <input
         type="checkbox"
         checked={task.completed || false} readOnly
-        className="w-8 h-3 cursor-pointer"
+        className="w-6 h-6 appearance-none cursor-pointer rounded border-2 border-[#F15A2B] bg-[#01013D] checked:bg-[#F15A2B] transition"
       />
-      <div className="flex-1">{task.title}</div>
-      <Image src={deleteIcon} alt="Delete Task" width={20} height={20} className="cursor-pointer" />
+      </div>
+
+      <div className="flex-1 text-base font-semibold leading-snug px-3">{task.title}</div>
+      <button type="button" className="px-9 py-2 rounded-lg font-semibold text-[#F1FAF5] bg-[#F15A2B] hover:bg-orange-600 transition-all shadow-md"
+        onClick={() => console.log(`Delete task with id: ${task.id}`)}
+      >
+        Delete
+      </button>
     </div>
   );
 }
