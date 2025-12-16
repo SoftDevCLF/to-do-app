c08fd666ce845e36a6cd0d2bdef2fe6fb4c65e2a
Temi Bankole <temibankole5@gmail.com>
2025-12-15 20:47:31 -0700

    Changing prop name

--- app/components/TaskList.js
+++ app/components/TaskList.js
@@ -1,27 +1,13 @@
 "use client";
 import TaskCard from "./TaskCard";
-import tasks from "../data/tasks.json";
 import { useState } from "react";
 
 //TaskList component displays a list of tasks
-export default function TaskList({filter, searchTerm}) {
+export default function TaskList({tasks, filter, searchTerm, onToggleComplete, onRequestDelete}) {
 
  //Make tasks reactive
   const [tasksList, setTaskList] = useState(tasks);
 
-  //Toggle a task's completion
-  const handleToggleComplete = (taskId) => {
-    setTasks(prev =>
-      prev.map(task =>
-        task.id === taskId ? { ...task, completed: !task.completed } : task
-      )
-    );
-  };
-
-  //Delete a task
-  const handleDeleteTask = (taskId) => {
-    setTaskList(prev => prev.filter(task => task.id !== taskId));
-  };
 
   //Filter tasks
   const filteredTasks = tasksList
@@ -38,7 +24,11 @@ export default function TaskList({filter, searchTerm}) {
   return (
     <div className="px-10 ml-0 gap-x-20 grid grid-cols-1 md:grid-cols-2 gap-6">
       {filteredTasks.map(task => (
-        <TaskCard key={task.id} task={task} onToggleComplete={handleToggleComplete} onDeleteTask={handleDeleteTask} />
+        <TaskCard 
+        key={task.id}
+        task={task} 
+        onToggleComplete={onToggleComplete} 
+        onRequestDelete={onRequestDelete} />
       ))}
     </div>
   );
