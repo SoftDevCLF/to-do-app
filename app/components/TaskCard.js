317c82091583ce5a0aa117ef214a6aa8c06bbe63
Temi Bankole <temibankole5@gmail.com>
2025-12-15 22:12:17 -0700

    Styling for completed task

--- app/components/TaskCard.js
+++ app/components/TaskCard.js
@@ -4,17 +4,24 @@
 export default function TaskCard({ task, onToggleComplete, onRequestDelete, onRequestEdit }) {
   
   return (
-    <div className="flex items-center justify-between w-full p-8 mb-4 border border-[#F15A2B] rounded-2xl bg-[#01013D] text-white shadow-lg hover:shadow-xl hover:scale-105 transition-all ">
+    <div
+      className={`flex items-center justify-between w-full p-8 mb-4 rounded-2xl shadow-lg transition-all ${
+        task.completed ? "bg-[#010122] border border-[#ffffff]" : "bg-[#01013D] border border-[#F15A2B]"
+      }`}
+    >
       <div className="flex items-center gap-4 py-6">
+      {/* Checkbox */}
       <input
         type="checkbox"
         checked={task.completed || false}
-        className="w-6 h-6 appearance-none cursor-pointer rounded border-2 border-[#F15A2B] bg-[#01013D] checked:bg-[#F15A2B] transition"
-        onChange={() => onToggleComplete(task.id)}
+        className="w-6 h-6 cursor-pointer rounded border-2 border-[#F15A2B] bg-[#01013D] checked:bg-[#F15A2B] accent-[#F15A2B] transition"
+        onChange={() => onToggleComplete(task)}
+        aria-label={`Mark task "${task.title}" as ${task.completed ? "incomplete" : "complete"}`}
       />
       </div>
       <div className="flex gap-4 items-center flex-1">
-        <div className="flex-1 text-base font-semibold leading-snug px-3">{task.title}</div>
+        <div className={`flex-1 text-base font-semibold leading-snug px-3 ${task.completed ? "line-through text-gray-400" : "text-[#F1FAF5]"}`}>{task.title}</div>
+
         {/* Edit Button */}
         <button type="button" className="px-4 py-2 rounded-lg font-semibold text-[#F1FAF5] bg-[#F15A2B] hover:bg-orange-600 hover:scale-105 transition-all shadow-md"
          onClick={() => onRequestEdit(task)}
