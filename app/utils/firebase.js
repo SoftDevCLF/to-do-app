e0eb8afcbf0c4fbc8bccdb0c4b1f4125bd5735e8
isayabs <isabelleyabut@gmail.com>
2025-12-16 23:22:28 -0700

    Export Firestore database instance

--- app/utils/firebase.js
+++ app/utils/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDwjw2QvBy9PO7hQYYPrYd7L08cFKa-FFs",
@@ -12,3 +13,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
