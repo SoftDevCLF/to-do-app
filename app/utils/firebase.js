1398222a7adba969983bea6e0a87a131615418e9
SoftDevCLF <cintya.lara.f@gmail.com>
2025-12-16 23:36:29 -0700

    Change firebase config to ensure api key security.

--- app/utils/firebase.js
+++ app/utils/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDwjw2QvBy9PO7hQYYPrYd7L08cFKa-FFs",
-  authDomain: "listo-8656b.firebaseapp.com",
-  projectId: "listo-8656b",
-  storageBucket: "listo-8656b.firebasestorage.app",
-  messagingSenderId: "557700412466",
-  appId: "1:557700412466:web:ef379e0e0af18dd0d9b82b"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
