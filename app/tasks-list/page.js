04dfa2c1b4c1a9774c11ab832b3c2443374d3b44
SoftDevCLF <cintya.lara.f@gmail.com>
2025-12-16 17:48:21 -0700

    Align elements of the search bar and add task button

--- app/tasks-list/page.js
+++ app/tasks-list/page.js
@@ -15,9 +15,7 @@ import testTasks from "../data/tasks.json";
 import Confetti from "react-confetti";
 //import { getTasks } from "./_services/taskService";
 
-
 export default function TaskListPage() {
-
   const { user } = useAuth();
   const router = useRouter();
 
@@ -29,7 +27,7 @@ export default function TaskListPage() {
   const [taskToDelete, setTaskToDelete] = useState(null);
   const [taskToEdit, setTaskToEdit] = useState(null);
   const [showConfetti, setShowConfetti] = useState(false);
-  
+
   useEffect(() => {
     if (!user) {
       router.push("/tasks-list/login");
@@ -37,29 +35,28 @@ export default function TaskListPage() {
   }, [user, router]);
 
   if (!user) return null;
-  
 
-  //CRUD FUNCTIONS TO BE IMPLEMENTED WITH BACKEND API/DATABASE SERVICES CALLS 
+  //CRUD FUNCTIONS TO BE IMPLEMENTED WITH BACKEND API/DATABASE SERVICES CALLS
   //Handle adding a new task
   const handleAddTask = (newTask) => {
-  setTasks((prev) => [...prev, newTask]);
-  setShowModal(false);                    
+    setTasks((prev) => [...prev, newTask]);
+    setShowModal(false);
   };
 
   //Handle toggling task completion
   const handleToggleComplete = (task) => {
-  // Toggle task completion
-  setTasks(prev =>
-    prev.map(t =>
-      t.id === task.id ? { ...t, completed: !t.completed } : t
-    )
-  );
-  //Trigger confetti when task is being completed
-  if (!task.completed) {
-    setShowConfetti(true);
-    setTimeout(() => setShowConfetti(false), 2000); // confetti lasts 2s
-  }
-};
+    // Toggle task completion
+    setTasks((prev) =>
+      prev.map((t) =>
+        t.id === task.id ? { ...t, completed: !t.completed } : t
+      )
+    );
+    //Trigger confetti when task is being completed
+    if (!task.completed) {
+      setShowConfetti(true);
+      setTimeout(() => setShowConfetti(false), 2000); // confetti lasts 2s
+    }
+  };
 
   //Hande request delete to open modal
   const handleRequestDelete = (task) => {
@@ -70,14 +67,14 @@ export default function TaskListPage() {
   const handleConfirmDelete = (taskId) => {
     setTasks((prev) => prev.filter((task) => task.id !== taskId));
     setTaskToDelete(null);
-  }
+  };
 
   //Handle edit task
   const handleRequestEdit = (task) => {
     setTaskToEdit(task);
   };
 
-  //Handle update task 
+  //Handle update task
   const handleUpdateTask = (updatedTask) => {
     setTasks((prev) =>
       prev.map((task) => (task.id === updatedTask.id ? updatedTask : task))
@@ -97,21 +94,27 @@ export default function TaskListPage() {
 
         <main className="flex max-w-screen w-full flex-col items-center bg-[#000024] sm:items-start">
           {/*Render confetti on task completion */}
-          {showConfetti && <Confetti 
-            numberOfPieces={1000}
-            recycle={false}
-            colors={["#F15A2B","#F1FAF5","#01013D"]} />}
+          {showConfetti && (
+            <Confetti
+              numberOfPieces={1000}
+              recycle={false}
+              colors={["#F15A2B", "#F1FAF5", "#01013D"]}
+            />
+          )}
           <section className="w-full text-center sm:text-left gap-5">
-            <h1 className="text-5xl font-bold mb-6 text-[#F1FAF5] py-12 ps-10">
+            <h1 className="text-5xl font-bold mb-6 text-[#F1FAF5] py-12 ps-22">
               Task List
             </h1>
           </section>
-          <div className="w-full flex items-center justify-between gap-4 px-10 mb-4">
+          <div className="w-full flex items-center justify-between gap-4 px-22 mb-4">
             <SearchBar value={searchTerm} onChange={setSearchTerm} />
             {/* Add task button and modal */}
-              <button onClick={() => setShowModal(true)} className="mr-4 px-3 py-3 rounded-lg font-semibold text-[#F1FAF5] bg-[#F15A2B] hover:bg-orange-600 transition-all shadow-md">
+            <button
+              onClick={() => setShowModal(true)}
+              className="px-3 py-3 rounded-lg font-semibold text-[#F1FAF5] bg-[#F15A2B] hover:bg-orange-600 transition-all shadow-md"
+            >
               Add New Task
-              </button>
+            </button>
           </div>
           {showModal && (
             <TaskForm
@@ -119,17 +122,17 @@ export default function TaskListPage() {
               onClose={() => setShowModal(false)}
             />
           )}
-          <TaskList 
-            tasks={tasks} 
-            filter={filteredItems} 
-            searchTerm={searchTerm} 
-            onRequestDelete={handleRequestDelete} 
+          <TaskList
+            tasks={tasks}
+            filter={filteredItems}
+            searchTerm={searchTerm}
+            onRequestDelete={handleRequestDelete}
             onRequestEdit={handleRequestEdit}
-            onToggleComplete={handleToggleComplete} 
-            />
+            onToggleComplete={handleToggleComplete}
+          />
           {taskToDelete && (
             <ConfirmDeleteModal
-              task={taskToDelete} 
+              task={taskToDelete}
               onConfirm={handleConfirmDelete}
               onCancel={() => setTaskToDelete(null)}
             />
