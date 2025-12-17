0c1b9f487bde7b3e91b8b4427e902c995fede54f
SoftDevCLF <cintya.lara.f@gmail.com>
2025-12-17 03:47:48 -0700

    Rename files

--- app/components/SideNavBar.js
+++ app/components/SideNavBar.js
@@ -2,7 +2,7 @@ export default function SideNavBar({ onClick }) {
   return (
     <div className="w-1/9 bg-[#01013d] border-r border-[#011D33] flex flex-col items-center py-12">
       <ul className="flex flex-col space-y-6 text-white text-xl font-bold text-center">
-        <li className="border-b border-[#F15A2B] pb-8 ms-5 me-5">
+        <li className="border-b border-purple-600 pb-8 ms-5 me-5">
           <button
             onClick={() => onClick("all")}
             className="px-4 py-2 bg-[#01013d] cursor-pointer"
@@ -10,7 +10,7 @@ export default function SideNavBar({ onClick }) {
             All Tasks
           </button>
         </li>
-        <li className="border-b border-[#F15A2B] pb-8 ms-5 me-5">
+        <li className="border-b border-purple-600 pb-8 ms-5 me-5">
           <button
             onClick={() => onClick("completed")}
             className="px-4 py-2 bg-[#01013d] cursor-pointer"
@@ -18,7 +18,7 @@ export default function SideNavBar({ onClick }) {
             Completed Tasks
           </button>
         </li>
-        <li className="border-b border-[#F15A2B] pb-8 ms-5 me-5">
+        <li className="border-b border-purple-600 pb-8 ms-5 me-5">
           <button
             onClick={() => onClick("incomplete")}
             className="px-4 py-2 bg-[#01013d] cursor-pointer"
